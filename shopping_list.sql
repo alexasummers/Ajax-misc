dc4ac30a05f2027c8dbe4fac0933cbe94e00e3ea
Alexa Summers <alexas0202@gmail.com>
2020-11-28 22:00:39 -0600

    Added Shopping list PHP/JS and SQL

---
+++ shopping_list.sql
@@ -0,0 +1,14 @@
+CREATE TABLE Item (
+ID INT NOT NULL AUTO_INCREMENT,
+Name VARCHAR(255) NOT NULL,
+Completed Boolean,
+PRIMARY KEY (ID)
+);
+
+
+INSERT INTO Item(Name,Completed)
+VALUES ('Fruits',FALSE),
+('Vegetables',TRUE),
+('Milk',False);
+
+
